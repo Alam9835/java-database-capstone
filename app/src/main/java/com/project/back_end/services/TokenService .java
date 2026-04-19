54aeab35734a8e02596126f736aaf5d6afc995d0
Shahbaz Alam <88643897+Alam9835@users.noreply.github.com>
2026-04-19 15:52:25 +0530

    Update TokenService .java

--- app/src/main/java/com/project/back_end/services/TokenService .java
+++ app/src/main/java/com/project/back_end/services/TokenService .java
@@ -1,11 +1,25 @@
 package com.project.back_end.services;
 
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
 
 @Service
 public class TokenService {
 
+    private final String SECRET_KEY = "mysecretkey123";
+
     public String generateToken(String email) {
-        return "token_for_" + email;
+
+        long currentTime = System.currentTimeMillis();
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(currentTime + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
     }
 }
